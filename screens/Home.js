d4f68453f98452d5e696f323d5f92f7b449f40bf
Furkan Baykal <furkanbaykal2014@hotmail.com>
2022-06-19 14:05:47 +0200

    master

--- screens/Home.js
+++ screens/Home.js
@@ -1,21 +1,10 @@
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
-import { ScrollView } from "react-native";
-import Categories from "../components/Categories";
-import HeaderTabs from "../components/HeaderTabs";
-import RestaurantItem from "../components/RestaurantItem";
-import SearchBar from "../components/SearchBar";
+import { View, Text } from "react-native";
 
 export default function Home() {
     return (
-        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
-            <View style={{ backgroundColor: "white", padding: 15 }}>
-                <HeaderTabs />
-                <SearchBar />
-            </View>
-            <ScrollView showsVerticalScrollIndicator={false} />  {/* scrolling by touching the screen so everything moves */}
-                <RestaurantItem />
-            <ScrollView />
-        </SafeAreaView>
+        <View>
+            <HeaderTabs />
+        </View>
     );
 }
