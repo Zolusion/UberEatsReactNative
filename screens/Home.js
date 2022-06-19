e814a8e2609a4f61fa95c0020c789f50d5076248
Furkan Baykal <furkanbaykal2014@hotmail.com>
2022-06-19 09:10:30 +0200

    master

--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
+import { ScrollView } from "react-native-web";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 
 export default function Home() {
     return (
-        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1}}>
-            <View style={{ backgroundColor: "white", padding: 15}}>
-                <HeaderTabs/>
+        <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
+            <View style={{ backgroundColor: "white", padding: 15 }}>
+                <HeaderTabs />
                 <SearchBar />
             </View>
-            <Categories/>
+            <ScrollView showsVerticalScrollIndicator={false} />
+                <Categories />
+            <ScrollView />
         </SafeAreaView>
     );
 }
