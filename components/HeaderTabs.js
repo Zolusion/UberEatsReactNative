9cc09581eb5ff7e77c130f6be4c3cf3b439e68e1
Furkan Baykal <furkanbaykal2014@hotmail.com>
2022-06-21 08:33:18 +0200

    master

--- components/HeaderTabs.js
+++ components/HeaderTabs.js
@@ -2,35 +2,36 @@ import React, { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 
 
-export default function HeaderTabs() {
-    const [activeTab, setActiveTab] = useState("Delivery")
+export default function HeaderTabs(props) {
     return (
         <View style={{ flexDirection: "row", alignSelf: "center" }}>
-            <HeaderButton 
-            text="Delivery" 
-            btnColor="black" 
-            textColor="white" 
-            activeTab={activeTab} 
-            setActiveTab={setActiveTab}/> 
-            <HeaderButton 
-            text="Pickup" 
-            btnColor="white" 
-            textColor="black"
-            activeTab={activeTab} 
-            setActiveTab={setActiveTab}/> 
+            <HeaderButton
+                text="Delivery"
+                btnColor="black"
+                textColor="white"
+                activeTab={props.activeTab}
+                setActiveTab={props.setActiveTab}
+            />
+            <HeaderButton
+                text="Pickup"
+                btnColor="white"
+                textColor="black"
+                activeTab={props.activeTab}
+                setActiveTab={props.setActiveTab}
+            />
         </View>
     );
 }
 
 const HeaderButton = (props) => (
     <TouchableOpacity style={{
-        backgroundColor: props.activeTab === props.text ? "black" : "white", 
+        backgroundColor: props.activeTab === props.text ? "black" : "white",
         paddingVertical: 6,
         paddingHorizontal: 16,
         borderRadius: 30,
     }}
-    onPress={() => props.setActiveTab(props.text)} 
-    > 
+        onPress={() => props.setActiveTab(props.text)}
+    >
         <Text style={{
             color: props.activeTab === props.text ? "white" : "black",
             fontSize: 15,
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, SafeAreaView, ScrollView } from "react-native";
+import { Divider } from "react-native-elements";
+import BottomTabs from "../components/BottomTabs";
 import Categories from "../components/Categories";
 import HeaderTabs from "../components/HeaderTabs";
 import RestaurantItems, { localRestaurants } from "../components/RestaurantItems";
@@ -10,6 +12,7 @@ const YELP_API_KEY = "OnuYS3nbICH8joQ9spRUbzO3NBYsbapyMzzXqhqqH4UqX4ING4_zfcVkTs
 export default function Home() {
     const [restaurantData, setRestaurantData] = React.useState(localRestaurants);
     const [city, setCity] = useState("San Francisco");
+    const [activeTab, setActiveTab] = useState("Delivery")
 
     const getRestaurantsFromYelp = () => {
         const yelpUrl = `https://api.yelp.com/v3/businesses/search?term=restaurants&location=${city}`;
@@ -22,12 +25,19 @@ export default function Home() {
 
         return fetch(yelpUrl, apiOptions)
             .then((res) => res.json())
-            .then((json) => setRestaurantData(json.businesses));
+            .then((json) =>
+                setRestaurantData(
+                    json.businesses.filter((business) => business.transactions.includes
+                        (activeTab.toLowerCase()
+                        )
+                    )
+                )
+            );
     };
 
     useEffect(() => {
         getRestaurantsFromYelp();
-    }, [city])
+    }, [city, activeTab]);
 
     return (
         <SafeAreaView style={{
@@ -38,13 +48,15 @@ export default function Home() {
                 backgroundColor: "white",
                 padding: 15
             }}>
-                <HeaderTabs />
+                <HeaderTabs activeTab={activeTab} setActiveTab={setActiveTab}/>
                 <SearchBar cityHandler={setCity} />
             </View>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <Categories />
                 <RestaurantItems restaurantData={restaurantData} />
             </ScrollView>
+            <Divider width={1} />
+            <BottomTabs />
         </SafeAreaView>
     );
 }
